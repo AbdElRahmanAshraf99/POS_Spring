3d59502f9eed17e3dba3f2996052561c60b18133
AbdElRahmanAshraf99 <abdoalaraby74@outlook.com>
2022-12-13 20:16:11 +0200

    First Commit

---
+++ src/test/java/com/pos/PosApplicationTests.java
@@ -0,0 +1,15 @@
+package com.pos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
